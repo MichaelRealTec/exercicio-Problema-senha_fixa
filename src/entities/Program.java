739d72b6bc0a5d4cf2390a03e018b81ed23ad3d4
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-07 02:54:40 -0300

    Exercicio resolvido da senha_fixa

--- src/entities/Program.java
+++ src/entities/Program.java
@@ -1,10 +1,28 @@
 package entities;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// Exercicio senha_fixa
 		
+		int senha;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite a senha: ");
+		senha = sc.nextInt();
+		
+		while(senha != 2002) {
+			System.out.print("Senha Invalida! Tente novamente: ");
+			senha = sc.nextInt();
+		}
+		System.out.println("Acesso permitido!");
+		
+		
+		
+		sc.close();
 
 	}
 
